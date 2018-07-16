42ec7b2969f237f1cf40f15aa4ea2a4696530403
Hiếu <hieuntse62694@gmail.com>
2018-07-16 23:45:30 +0700

    13/7/2018 version 4

--- WebApplication1/Controllers/CartController.cs
+++ WebApplication1/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
@@ -22,6 +23,41 @@ namespace WebApplication1.Controllers
             }
             return View(list);
         }
+        public JsonResult DeleteAll(string cartModel)
+        {
+            Session[CartSession] = null;
+            return Json(new
+            {
+                status = true
+            });
+        }
+        public JsonResult Update(string cartModel)
+        {
+            var jsoncart = new JavaScriptSerializer().Deserialize<List<CartItem>>(cartModel);
+            var sessioncart = (List<CartItem>)Session[CartSession];
+            foreach(var item in sessioncart)
+            {
+                var jsonitem = jsoncart.SingleOrDefault(x => x.food.id == item.food.id);
+                if (jsonitem !=null)
+                {
+                    item.quantity = jsonitem.quantity;
+                }
+            }
+            return Json(new
+            {
+                status = true
+            });
+        }
+        //public JsonResult Remove(string cartModel)
+        //{
+        //    var jsoncart = new JavaScriptSerializer().Deserialize<List<CartItem>>(cartModel);
+        //    var sessioncart = (List<CartItem>)Session[CartSession];
+        //    int index = sessioncart.Remove(jsoncart);
+        //    return Json(new
+        //    {
+        //        status = true
+        //    });
+        //}
         public ActionResult AddItem(int id,int quantity)
         {
             var fooddetail = db.foods.Where(x => x.id == id).SingleOrDefault();
@@ -61,5 +97,63 @@ namespace WebApplication1.Controllers
             }
             return RedirectToAction("Index");
         }
+        //public ActionResult getAllIdFoodInMenu(int id)
+        //{
+        //    var id = db.
+        //}
+        //public ActionResult AddAllFoodInMenu(int id, int quantity)
+        //{
+        //    var menu = from f in db.foods
+        //                           join md in db.menu_detail
+        //                           on f.id equals md.id_food
+        //                           join m in db.menus
+        //                           on md.id_menu equals m.id
+        //                           where md.id_menu == id
+        //                           select new MenuView()
+        //                           {
+        //                               menuName = m.name,
+        //                               image = m.image,
+        //                               foodName = f.name,
+        //                               price = f.price,
+        //                               sumary = f.summary,
+        //                               id = m.id
+        //                           };
+        //    var fooddetail = menu.SingleOrDefault();
+        //    var cart = Session[CartSession];
+        //    if (cart != null)
+        //    {
+        //        var list = (List<CartItem>)cart;
+        //        if (list.Exists(x => x.food.id == id))
+        //        {
+        //            foreach (var item in list)
+        //            {
+        //                if (item.food.id == id)
+        //                {
+        //                    item.quantity += quantity;
+        //                }
+        //            }
+        //        }
+        //        else
+        //        {
+        //            //tạo mới đối tượng cart item
+        //            var item = new CartItem();
+        //            item.food = fooddetail;
+        //            item.quantity = quantity;
+        //            list.Add(item);
+        //        }
+
+        //    }
+        //    else
+        //    {
+        //        //tạo mới đối tượng cart item
+        //        var item = new CartItem();
+        //        item.food = fooddetail;
+        //        item.quantity = quantity;
+        //        var list = new List<CartItem>();
+        //        list.Add(item);
+        //        Session[CartSession] = list;
+        //    }
+        //    return RedirectToAction("Index");
+        //}
     }
 }
