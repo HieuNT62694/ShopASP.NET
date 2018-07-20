e2c506c9e925155bb9d5e1a9d40d20da3b261ecb
Hiếu <hieuntse62694@gmail.com>
2018-07-21 02:18:04 +0700

    21/7/2018 view bill

---
+++ WebApplication1/Controllers/BillController.cs
@@ -0,0 +1,37 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BillController : Controller
+    {
+        // GET: Bill
+        project2Entities db = new project2Entities();
+        public ActionResult Index()
+        {
+            //var listbill = listAllPaging(page, pagesize);
+           var model = from b in db.bills
+                        join cus in db.customers
+                           on b.id_customer equals cus.id
+                           where b.note.Equals("Chưa Giao")
+                                       select new BillCusModel()
+                                       {
+                                           idbill = b.id,
+                                           idcus = cus.id,
+                                           namecus = cus.name,
+                                           address = cus.address,
+                                           date = b.date_order,
+                                           phone = cus.phone,
+                                           price = b.total,
+                                           status = b.note
+                                       };
+            var listbill = model.ToList();
+            return View(listbill);
+        }
+    }
+}
---
+++ WebApplication1/Models/BillCusModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class BillCusModel
+    {
+        public int idbill { get; set; }
+        public int idcus { get; set; }
+        public string namecus { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public DateTime date { get; set; }
+        public double price { get; set; }
+        public string status { get; set; }
+    }
+}
